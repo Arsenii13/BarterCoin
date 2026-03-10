ce436bb9b08150fe9242573b18e8658d2aa6d2b9
Arsenii13 <arseniyonjegeniy@gmail.com>
2026-03-10 19:59:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"; 
 
 import {
 getAuth,
@@ -162,5 +162,6 @@ Price: ${item.price} BC
 container.appendChild(div);
 
 });
+
 
 }
