1d3f02ece21337c68a5d585301c38d13e0976b93
Arsenii13 <arseniyonjegeniy@gmail.com>
2026-03-11 18:37:43 +0200

    sss

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getAuth,
+GoogleAuthProvider,
+signInWithPopup,
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore,
+doc,
+setDoc,
+getDoc,
+updateDoc,
+collection,
+addDoc,
+getDocs,
+runTransaction
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 
 const firebaseConfig = {
-apiKey: "AIzaSyAqVEpAQ8sT15lLoWzJe0jmFGE3jsU_BTQ",
-authDomain: "bartercoin-3fc73.firebaseapp.com",
-projectId: "bartercoin-3fc73",
-storageBucket: "bartercoin-3fc73.appspot.com",
-messagingSenderId: "1047699487399",
-appId: "1:1047699487399:web:a54c50ac062f857a923982"
+
+apiKey:"PASTE",
+authDomain:"PASTE",
+projectId:"PASTE",
+storageBucket:"PASTE",
+messagingSenderId:"PASTE",
+appId:"PASTE"
+
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const app=initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const db=getFirestore(app);
 
 let currentUser=null;
+
 
 const loginBtn=document.getElementById("loginBtn");
 const logoutBtn=document.getElementById("logoutBtn");
@@ -23,7 +45,6 @@ const logoutBtn=document.getElementById("logoutBtn");
 const walletPage=document.getElementById("walletPage");
 const marketPage=document.getElementById("marketPage");
 const transferPage=document.getElementById("transferPage");
-
 const loginPage=document.getElementById("loginPage");
 
 const balanceEl=document.getElementById("balance");
@@ -31,6 +52,7 @@ const balanceEl=document.getElementById("balance");
 const navWallet=document.getElementById("navWallet");
 const navMarket=document.getElementById("navMarket");
 const navTransfer=document.getElementById("navTransfer");
+
 
 function showPage(page){
 
@@ -46,26 +68,36 @@ navWallet.onclick=()=>showPage(walletPage);
 navMarket.onclick=()=>showPage(marketPage);
 navTransfer.onclick=()=>showPage(transferPage);
 
+
+
 loginBtn.onclick=async()=>{
 
 const provider=new GoogleAuthProvider();
+
 const result=await signInWithPopup(auth,provider);
 
 const user=result.user;
 
-const userRef=doc(db,"users",user.uid);
-const snap=await getDoc(userRef);
+const ref=doc(db,"users",user.uid);
+
+const snap=await getDoc(ref);
 
 if(!snap.exists()){
-await setDoc(userRef,{
+
+await setDoc(ref,{
 name:user.displayName,
 balance:100
 });
+
 }
 
 };
 
+
+
 logoutBtn.onclick=()=>signOut(auth);
+
+
 
 onAuthStateChanged(auth,async(user)=>{
 
@@ -76,9 +108,7 @@ currentUser=user;
 loginPage.style.display="none";
 
 await loadBalance();
-
 await loadUsers();
-
 await loadOffers();
 
 showPage(walletPage);
@@ -91,6 +121,8 @@ loginPage.style.display="block";
 
 });
 
+
+
 async function loadBalance(){
 
 const snap=await getDoc(doc(db,"users",currentUser.uid));
@@ -98,6 +130,8 @@ const snap=await getDoc(doc(db,"users",currentUser.uid));
 balanceEl.textContent=snap.data().balance;
 
 }
+
+
 
 document.getElementById("postOfferBtn").onclick=async()=>{
 
@@ -118,112 +152,156 @@ loadOffers();
 
 };
 
+
+
 async function loadOffers(){
 
-const offersList=document.getElementById("offersList");
-offersList.innerHTML="";
+const list=document.getElementById("offersList");
 
-const querySnap=await getDocs(collection(db,"offers"));
+list.innerHTML="";
 
-querySnap.forEach(docu=>{
+const query=await getDocs(collection(db,"offers"));
+
+query.forEach(docu=>{
 
 const data=docu.data();
 
 const div=document.createElement("div");
+
 div.className="offer";
 
 div.innerHTML=`
+
 <h4>${data.title}</h4>
+
 <p>${data.desc}</p>
+
 Price: ${data.price} BC
-<br>
-<button data-id="${docu.id}">Buy</button>
+
+<br><br>
+
+<button>Buy</button>
+
 `;
 
-div.querySelector("button").onclick=()=>buyOffer(docu.id,data);
+div.querySelector("button").onclick=()=>buyOffer(data);
 
-offersList.appendChild(div);
+list.appendChild(div);
 
 });
 
 }
 
-async function buyOffer(id,data){
+
+
+async function buyOffer(data){
 
 if(data.seller===currentUser.uid){
-alert("You can't buy your own offer");
+
+alert("You cannot buy your own offer");
+
 return;
+
 }
 
 const buyerRef=doc(db,"users",currentUser.uid);
 const sellerRef=doc(db,"users",data.seller);
 
-const buyerSnap=await getDoc(buyerRef);
+try{
+
+await runTransaction(db,async(transaction)=>{
+
+const buyerSnap=await transaction.get(buyerRef);
+const sellerSnap=await transaction.get(sellerRef);
 
 let buyerBal=buyerSnap.data().balance;
 
 if(buyerBal<data.price){
-alert("Not enough coins");
-return;
+
+throw "Not enough coins";
+
 }
 
-await updateDoc(buyerRef,{balance:buyerBal-data.price});
+transaction.update(buyerRef,{
+balance:buyerBal-data.price
+});
 
-const sellerSnap=await getDoc(sellerRef);
-let sellerBal=sellerSnap.data().balance;
+transaction.update(sellerRef,{
+balance:sellerSnap.data().balance+data.price
+});
 
-await updateDoc(sellerRef,{balance:sellerBal+data.price});
-
-loadBalance();
+});
 
 alert("Purchase successful");
 
+loadBalance();
+
+}catch(e){
+
+alert(e);
+
 }
+
+}
+
+
 
 async function loadUsers(){
 
 const select=document.getElementById("userSelect");
+
 select.innerHTML="";
 
-const querySnap=await getDocs(collection(db,"users"));
+const query=await getDocs(collection(db,"users"));
 
-querySnap.forEach(u=>{
+query.forEach(u=>{
 
 if(u.id===currentUser.uid)return;
 
-const option=document.createElement("option");
+const opt=document.createElement("option");
 
-option.value=u.id;
-option.textContent=u.data().name;
+opt.value=u.id;
+opt.textContent=u.data().name;
 
-select.appendChild(option);
+select.appendChild(opt);
 
 });
 
 }
 
+
+
 document.getElementById("sendBtn").onclick=async()=>{
 
 const uid=document.getElementById("userSelect").value;
+
 const amount=parseInt(document.getElementById("transferAmount").value);
 
 const myRef=doc(db,"users",currentUser.uid);
 const otherRef=doc(db,"users",uid);
 
-const mySnap=await getDoc(myRef);
+await runTransaction(db,async(transaction)=>{
+
+const mySnap=await transaction.get(myRef);
+const otherSnap=await transaction.get(otherRef);
+
 let myBal=mySnap.data().balance;
 
 if(myBal<amount){
-alert("Not enough coins");
-return;
+
+throw "Not enough coins";
+
 }
 
-await updateDoc(myRef,{balance:myBal-amount});
+transaction.update(myRef,{
+balance:myBal-amount
+});
 
-const otherSnap=await getDoc(otherRef);
-let otherBal=otherSnap.data().balance;
+transaction.update(otherRef,{
+balance:otherSnap.data().balance+amount
+});
 
-await updateDoc(otherRef,{balance:otherBal+amount});
+});
 
 loadBalance();
 
